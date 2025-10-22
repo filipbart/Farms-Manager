55f29eff04945137dc3553dfb4ed5561d9ec5e7e
Filip B <filipbart1@gmail.com>
2025-10-22 11:11:07 +0200

    fix(main): remove unnecessary UserEntity relationships from SaleInvoice configuration

--- backend/src/FarmsManager.Infrastructure/EntityConfigurations/SaleAggregate/SaleInvoiceEntity.Configuration.cs
+++ backend/src/FarmsManager.Infrastructure/EntityConfigurations/SaleAggregate/SaleInvoiceEntity.Configuration.cs
@@ -1,6 +1,4 @@
 using FarmsManager.Domain.Aggregates.SaleAggregate.Entities;
-using FarmsManager.Domain.Aggregates.UserAggregate.Entities;
-using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Metadata.Builders;
 
 namespace FarmsManager.Infrastructure.EntityConfigurations.SaleAggregate;
@@ -16,9 +14,5 @@ public class SaleInvoiceEntityConfiguration : BaseConfiguration<SaleInvoiceEntit
         builder.HasOne(t => t.Cycle).WithMany().HasForeignKey(t => t.CycleId);
         builder.HasOne(t => t.Farm).WithMany().HasForeignKey(t => t.FarmId);
         builder.HasOne(t => t.Slaughterhouse).WithMany().HasForeignKey(t => t.SlaughterhouseId);
-        
-        builder.HasOne<UserEntity>(t => t.Creator).WithMany().HasForeignKey(t => t.CreatedBy).OnDelete(DeleteBehavior.Restrict);
-        builder.HasOne<UserEntity>(t => t.Modifier).WithMany().HasForeignKey(t => t.ModifiedBy).OnDelete(DeleteBehavior.Restrict);
-        builder.HasOne<UserEntity>(t => t.Deleter).WithMany().HasForeignKey(t => t.DeletedBy).OnDelete(DeleteBehavior.Restrict);
     }
 }
