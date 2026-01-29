9d2b56460d24db79419b62df7d5c5a78d456acb8
Bart <filipbart1@gmail.com>
2026-01-29 09:23:13 +0100

    Add migration

---
+++ backend/src/FarmsManager.Infrastructure/Migrations/20260129082259_AdjustAssignmentEmployeeRules.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace FarmsManager.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class AdjustAssignmentEmployeeRules : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "fk_invoice_assignment_rules_farm_entity_farm_id",
+                schema: "farms_manager",
+                table: "invoice_assignment_rules");
+
+            migrationBuilder.DropIndex(
+                name: "ix_invoice_assignment_rules_farm_id",
+                schema: "farms_manager",
+                table: "invoice_assignment_rules");
+
+            migrationBuilder.DropColumn(
+                name: "farm_id",
+                schema: "farms_manager",
+                table: "invoice_assignment_rules");
+
+            migrationBuilder.AddColumn<string>(
+                name: "farm_ids",
+                schema: "farms_manager",
+                table: "invoice_assignment_rules",
+                type: "jsonb",
+                nullable: false,
+                defaultValue: "[]");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "farm_ids",
+                schema: "farms_manager",
+                table: "invoice_assignment_rules");
+
+            migrationBuilder.AddColumn<Guid>(
+                name: "farm_id",
+                schema: "farms_manager",
+                table: "invoice_assignment_rules",
+                type: "uuid",
+                nullable: true);
+
+            migrationBuilder.CreateIndex(
+                name: "ix_invoice_assignment_rules_farm_id",
+                schema: "farms_manager",
+                table: "invoice_assignment_rules",
+                column: "farm_id");
+
+            migrationBuilder.AddForeignKey(
+                name: "fk_invoice_assignment_rules_farm_entity_farm_id",
+                schema: "farms_manager",
+                table: "invoice_assignment_rules",
+                column: "farm_id",
+                principalSchema: "farms_manager",
+                principalTable: "farm",
+                principalColumn: "id",
+                onDelete: ReferentialAction.SetNull);
+        }
+    }
+}
